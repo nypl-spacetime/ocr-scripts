48463ed612705d9add51a8548eed5063c8df807f
Bert Spaan <bert.spaan@gmail.com>
2016-09-23 17:23:45 -0400

    two columns!

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ mkdir -p $OUTPUT_DIR/temp
 ocropus-nlbin -n -t 0.65 -Q 4 -o "$OUTPUT_DIR/temp" $@
 
 # Page segmentation
-ocropus-gpageseg -n --maxlines 1200 --maxseps 4 -b -Q 1 -d "$OUTPUT_DIR/temp/????.bin.png"
+ocropus-gpageseg -n --maxlines 1200 --maxseps 2 -b -Q 1 -d "$OUTPUT_DIR/temp/????.bin.png"
 
 # OCR
 ocropus-rpred -n -m "$SCRIPT_DIR/models/cd_training_gold_master_117000.pyrnn.gz" "$OUTPUT_DIR/temp/????/??????.bin.png"
