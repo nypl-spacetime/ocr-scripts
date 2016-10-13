ef4915f68bf0b6dc9043416e24d6ec3b55a50c65
Bert Spaan <bert.spaan@gmail.com>
2016-10-13 11:36:21 -0400

    CORES!

--- run.sh
+++ run.sh
@@ -3,16 +3,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR=$(dirname $1)
 
+NUM_CORES=$(getconf _NPROCESSORS_ONLN)
+USE_CORES=$(($NUM_CORES - 1))
+
 mkdir -p $OUTPUT_DIR/temp
 
 # Binarization
-ocropus-nlbin -n -t 0.65 -Q 4 -o "$OUTPUT_DIR/temp" $@
+ocropus-nlbin -n -t 0.65 -Q $USE_CORES -o "$OUTPUT_DIR/temp" $@
 
 # Page segmentation
-ocropus-gpageseg -n --maxlines 1200 --maxseps 2 -b -Q 1 -d "$OUTPUT_DIR/temp/????.bin.png"
+ocropus-gpageseg -n --maxlines 1200 --maxseps 2 -b -Q $USE_CORES -d "$OUTPUT_DIR/temp/????.bin.png"
 
 # OCR
-ocropus-rpred -n -m "$SCRIPT_DIR/models/cd_training_gold_master_117000.pyrnn.gz" "$OUTPUT_DIR/temp/????/??????.bin.png"
+ocropus-rpred -Q $USE_CORES -n -m "$SCRIPT_DIR/models/cd_training_gold_master_117000.pyrnn.gz" "$OUTPUT_DIR/temp/????/??????.bin.png"
 
 # Create hOCR file
 ocropus-hocr "$OUTPUT_DIR/temp/????.bin.png" -o "$OUTPUT_DIR/hocr.html"
